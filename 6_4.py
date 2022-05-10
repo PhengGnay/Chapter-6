b1fe88bc20d60c7847224b60b56ec06a14703d44
Pheng <phengyangster9@gmail.com>
2022-05-10 14:58:27 -0700

    adding 6-4

---
+++ 6_4.py
@@ -0,0 +1,20 @@
+# def power(r, n):
+#     print("form power()") 
+#     value = 1
+#     for i in range(1, n + 1):
+#         value = r * value
+#     return value
+# print("result", power(2, 3)) 
+
+
+# def power(r, n):
+#     if n == 1:
+#        return r
+#     return r * power(r, n - 1)  # same thing as the code under it
+# print("result", power(2, 3)) 
+
+
+# def power(r, n):
+#      return r if n == 1 else r * power(r, n - 1) # same code but all in one line
+# print("result", power(2, 3)) 
+
